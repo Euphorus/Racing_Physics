9148c2a59d89cbe435da077fb6c18b4fffdae51b
Yuvraj Singh Kumpavat <115008153+Euphorus@users.noreply.github.com>
2024-11-04 10:39:29 +0000

    minor fixes
    Happens, we're just getting started

--- Racing_Physics.cpp
+++ Racing_Physics.cpp
@@ -9,7 +9,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "Let's GOOOO", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -24,6 +24,8 @@ int main(void)
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
